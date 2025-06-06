7338b803416ebf24da7185b2c6e212a0e3d4c3ee
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-06 16:48:32 +0300

    room

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Room(models.Model):
+    number = models.IntegerField()
+    capacity = models.IntegerField()
+    location = models.TextField()
+
+    def __str__(self):
+        return f"Room # {self.number} - {self.capacity}"
+    
+
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+        ordering = ["number"]
+
+
+        
+class Booking(models.Model):
+    pass
+
+
