5bb68e719a6a0c70e59f2feb5f375a514b0149dc
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-12 10:47:31 +0300

    56

--- app/models.py
+++ app/models.py
@@ -25,6 +25,9 @@ class Booking(models.Model):
     end_time = models.DateTimeField()
     creation_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.room}"
+
     class Meta:
         verbose_name = "Booking"
         verbose_name_plural = "Booking"
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    text = "xui"
+
+    context = {
+
+        'all_text': text
+    }
+    return render(
+        request,
+        context=context,
+        template_name="booking/index.html"
+
+    )
